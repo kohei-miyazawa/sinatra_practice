2e2a0a2bcbc3b53f8e31ac61ca1db1874c92f305
kohei-miyazawa <kohei.miyazawa120@gmail.com>
2020-11-12 23:01:01 +0900

    詳細表示機能実装

--- app.rb
+++ app.rb
@@ -12,6 +12,10 @@ class Memo
     h = { id: SecureRandom.uuid, title: title, body: body }
     File.open("model/#{h[:id]}.json", "w") { |f| f.puts JSON.pretty_generate(h) }
   end
+
+  def self.find(id: memo_id)
+    JSON.parse(File.read("model/#{id}.json"), symbolize_names: true)
+  end
 end
 
 get "/" do
@@ -27,4 +31,9 @@ end
 post "/memos/new" do
   Memo.create(title: params[:title], body: params[:body])
   redirect to("/")
+end
+
+get "/memos/:id" do
+  @memo = Memo.find(id: params[:id])
+  erb :show
 end
