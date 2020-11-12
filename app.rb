3d6522ad9043fb4f6deb84fa2c1f842f391b84b6
kohei-miyazawa <kohei.miyazawa120@gmail.com>
2020-11-12 23:23:20 +0900

    rubocop自動修正

--- app.rb
+++ app.rb
@@ -1,17 +1,17 @@
 # frozen_string_literal: true
 
-require "bundler/setup"
-require "sinatra"
-require "sinatra/reloader"
-require "securerandom"
-require "json"
-require "pry"
+require 'bundler/setup'
+require 'sinatra'
+require 'sinatra/reloader'
+require 'securerandom'
+require 'json'
+require 'pry'
 
 class Memo
   class << self
     def create(title: memo_title, body: memo_body)
       h = { id: SecureRandom.uuid, title: title, body: body }
-      File.open("model/#{h[:id]}.json", "w") { |f| f.puts JSON.pretty_generate(h) }
+      File.open("model/#{h[:id]}.json", 'w') { |f| f.puts JSON.pretty_generate(h) }
     end
 
     def find(id: memo_id)
@@ -20,7 +20,7 @@ class Memo
 
     def update(id: memo_id, title: memo_title, body: memo_body)
       h = { id: id, title: title, body: body }
-      File.open("model/#{h[:id]}.json", "w") { |f| f.puts JSON.pretty_generate(h) }
+      File.open("model/#{h[:id]}.json", 'w') { |f| f.puts JSON.pretty_generate(h) }
     end
 
     def destroy(id: memo_id)
@@ -29,37 +29,37 @@ class Memo
   end
 end
 
-get "/" do
-  files = Dir.glob("model/*")
+get '/' do
+  files = Dir.glob('model/*')
   @memos = files.map { |file| JSON.parse(File.read(file), symbolize_names: true) }
   erb :index
 end
 
-get "/memos/new" do
+get '/memos/new' do
   erb :new
 end
 
-post "/memos/new" do
+post '/memos/new' do
   Memo.create(title: params[:title], body: params[:body])
-  redirect to("/")
+  redirect to('/')
 end
 
-get "/memos/:id" do
+get '/memos/:id' do
   @memo = Memo.find(id: params[:id])
   erb :show
 end
 
-get "/memos/edit/:id" do
+get '/memos/edit/:id' do
   @memo = Memo.find(id: params[:id])
   erb :edit
 end
 
-patch "/memos/:id" do
+patch '/memos/:id' do
   Memo.update(id: params[:id], title: params[:title], body: params[:body])
   redirect to("/memos/#{params[:id]}")
 end
 
-delete "/memos/:id" do
+delete '/memos/:id' do
   Memo.destroy(id: params[:id])
-  redirect to("/")
+  redirect to('/')
 end
